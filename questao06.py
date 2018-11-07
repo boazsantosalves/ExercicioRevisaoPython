778e57aa2920c32120501b1a111feabab27d0193
boazsantosalves <44641444+boazsantosalves@users.noreply.github.com>
2018-11-06 23:01:25 -0200

    Sexta questão da lista de revisão

---
+++ questao06.py
@@ -0,0 +1,8 @@
+nota1 = float(input("☺ Digite sua primeira nota : "))
+nota2 = float(input("☺ Digite sua segunda nota : "))
+nota3 = float(input("☺ Digite sua terceira nota : "))
+nota4 = float(input("☺ Digite sua quarta nota : "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+print("Suas notas foram: %.1f, %.1f, %.1f e %.1f"%(nota1, nota2, nota3, nota4))
+print("Sua média é: %f"%(media))
